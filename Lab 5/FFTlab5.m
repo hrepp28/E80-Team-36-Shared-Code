dcee93ae8e7f1422fd4d05e0f76d5d58c4037be1
Angela Z <angzheng@g.hmc.edu>
2026-03-09 23:21:55 -0700

    Upload some files from lab 5 and linear regression script for lab 4

--- Lab 5/FFTlab5.m
+++ Lab 5/FFTlab5.m
@@ -1,8 +1,8 @@
 %Load in file from data 
-data = readmatrix(''); %add file name
+data = readmatrix('f318e_7.csv'); %add file name
 
-t = data( :,1); % check to see which row is which and switch if swapped, this is just coppied from the pigeon hw:)
-Xk = data( :,2);
+t = data(3:end,1); % check to see which row is which and switch if swapped, this is just coppied from the pigeon hw:)
+Xk = data(3:end,2);
 Fs = 1/(t(2)-t(1));
 N = length(Xk);
 
@@ -13,6 +13,7 @@ Xw = Xk.*w;
 %take abs value for magnitude
 Xabs = abs(X);
 
+clf
 figure(1)
 plot(f, Xabs)
 xlabel('Frequency [Hz]')
