66ed6842f934df00603ef28ae6174e3262e8ce53
hrepp28 <smile.more.hr@gmail.com>
2026-03-25 16:57:52 -0700

    new folder

--- lab 7/lab7_plotgps.m
+++ lab 7/lab7_plotgps.m
@@ -1,6 +1,54 @@
 clear
 clf
 
+% logreader.m
+% Use this script to read data from your micro SD card
+
+clear;
+%clf;
+
+filenum = '013'; % file number for the data you want to read
+infofile = strcat('INF', filenum, '.TXT');
+datafile = strcat('LOG', filenum, '.BIN');
+
+%% map from datatype to length in bytes
+dataSizes.('float') = 4;
+dataSizes.('ulong') = 4;
+dataSizes.('int') = 4;
+dataSizes.('int32') = 4;
+dataSizes.('uint8') = 1;
+dataSizes.('uint16') = 2;
+dataSizes.('char') = 1;
+dataSizes.('bool') = 1;
+
+%% read from info file to get log file structure
+fileID = fopen(infofile);
+items = textscan(fileID,'%s','Delimiter',',','EndOfLine','\r\n');
+fclose(fileID);
+[ncols,~] = size(items{1});
+ncols = ncols/2;
+varNames = items{1}(1:ncols)';
+varTypes = items{1}(ncols+1:end)';
+varLengths = zeros(size(varTypes));
+colLength = 256;
+for i = 1:numel(varTypes)
+    varLengths(i) = dataSizes.(varTypes{i});
+end
+R = cell(1,numel(varNames));
+
+%% read column-by-column from datafile
+fid = fopen(datafile,'rb');
+for i=1:numel(varTypes)
+    %# seek to the first field of the first record
+    fseek(fid, sum(varLengths(1:i-1)), 'bof');
+    
+    %# % read column with specified format, skipping required number of bytes
+    R{i} = fread(fid, Inf, ['*' varTypes{i}], colLength-varLengths(i));
+    eval(strcat(varNames{i},'=','R{',num2str(i),'};'));
+end
+fclose(fid);
+
+
 %load image 
 parsons = imread('GPS_test_2.png'); 
 pim = imshow(parsons);
@@ -18,17 +66,9 @@ xlabel('X Position [m]')
 title('X,Y Position Over Map of Path Traveled')
 
 hold on 
-
-%read files 
-filenum = ''; % file number for the data we want to read
-infofile = strcat('INF', filenum, '.TXT');
-xydata = strcat('LOG', filenum, '.BIN');
-
-%test points 
-x = xydata( :,1); 
-y = xydata( :,2);
-
-scatter(x,y,'green', LineWidth= 3)
+xnew = x-10
+ynew= y-17
+scatter(xnew, ynew,'green', LineWidth= 3)
 
 
 
