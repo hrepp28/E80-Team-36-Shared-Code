7b132c6f36cbd7f49094e0c16d107c84585cac81
szarazua <szarazua@g.hmc.edu>
2026-03-25 16:13:20 -0700

    edits

--- lab 7/E80_Lab7_Section4_5.m
+++ lab 7/E80_Lab7_Section4_5.m
@@ -3,11 +3,11 @@
 imr = imread("E80IMG2_Lab7.jpg");
 
 % Scaling X and Y axis in meters
-Xmin = 107.00004;
-Xmax = 170;
+Xmin = 0;
+Xmax = 32.53;
 
-Ymin = -90;
-Ymax = -25;
+Ymin = 0;
+Ymax = 25.789128;
 
 % Plotting the image
 figure(1)
@@ -17,48 +17,33 @@ xlabel('X Measurement [meters]')
 ylabel('Y Measurement [meters]')
 title('Y Measurement vs. X Measurement')
 axis on;
-set(gca, 'YDir', 'normal')
 
 hold on 
 
-%% Log Reader
-% read files from micro SD Card
-filenum = '003'; % file number for the data you want to read
-infofile = strcat('INF', filenum, '.TXT');
-datafile = strcat('LOG', filenum, '.BIN');
 
-% Extracting Data from datafile 
-x = datafile( :,1); 
-y = datafile( :,2);
-yawe = datafile( :,3);
-u = datafile( :,4);
-
-
-% Setting Time Scale
-snum = length(datafile);
-Tsamp = #;
-t = Tsamp*snum;
-
-% Overlay logged paths of x and y points
-scatter(x,y,'blue', LineWidth= 2)
-
-% Plot of angle error vs. time. 
-figure(2)
-clf
-plot(t, yawe)
-xlabel('Time [insert]')
-ylabel('Angle Error [insert]')
-title('Time vs. Angle Error')
-
-% Plot of control effort vs. time 
-figure(3)
-clf
-plot(t, u)
-xlabel('Time [insert]')
-ylabel('Control Effot [insert]')
-title('Time vs. Control Effot')
-
-
-
-
+% % Setting Time Scale
+% snum = length(accelX);
+% Tsamp = 0.1;
+% t = Tsamp*snum;
+% t = 0:Tsamp:snum / 10;
+% tt = t';
+% 
+% % Overlay logged paths of x and y points
+% scatter(x,y,'blue', LineWidth= 2)
+% 
+% % Plot of angle error vs. time. 
+% figure(2)
+% clf
+% plot(t, yawe)
+% xlabel('Time [insert]')
+% ylabel('Angle Error [insert]')
+% title('Time vs. Angle Error')
+% 
+% % Plot of control effort vs. time 
+% figure(3)
+% clf
+% plot(t, u)
+% xlabel('Time [insert]')
+% ylabel('Control Effot [insert]')
+% title('Time vs. Control Effot')
 
